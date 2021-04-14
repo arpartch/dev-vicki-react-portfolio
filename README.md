ce7d33e46b1a53def0a9c901e56836295e667dcc
DrVicki <vbealman@fullsail.com>
2021-04-13 21:26:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-hyjtpt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-hyjtpt)
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import Pages from './pages';
+import "./style.css";
+
+function App() {
+  const [user, setUser] = useState(null);
+  useState(() => {
+    fetch ('https://gitconnected.com/v1/portfolio/drvicki')
+    .then(res => res.join())
+    .then(user => {
+      setUser(user);
+    });
+  }, []);
+if (!user) {
+  return <div />;
+}
+
+  return <Pages user={user} />;
+
+
+}
+
+export default App;
